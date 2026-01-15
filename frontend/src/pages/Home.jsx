81f87719a5e5e44242ac24d0d4d46a6fbc1f09a0
Prabin K.C. <kcprabin2063@gmail.com>
2026-01-15 22:51:15 +0545

    socket Initilized on frontend at Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // BACKEND URL
+const socket = io("http://localhost:8000"); // BACKEND URL
 
 const Home = () => {
 
