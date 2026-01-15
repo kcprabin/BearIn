9aa6ee3e26c2037f3b4579e7222483fcc16dada1
Prabin K.C. <kcprabin2063@gmail.com>
2026-01-15 22:20:47 +0545

    back to normal

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
-
 import app from "./app.js";
+
+
 import connectDB from "./database/conection.database.js";
-import { createServer } from "http";
-import { Server } from "socket.io";
 
-const PORT = process.env.PORT ;
-
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-});
-
-io.on("connection", (socket) => {
-  console.log("Socket connected:", socket.id);
-});
 
 try {
   await connectDB();
@@ -28,6 +13,6 @@ try {
   console.error("DB connect failed", err);
 }
 
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on ${process.env.PORT}`);
 });
